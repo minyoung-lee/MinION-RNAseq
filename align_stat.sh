a6f259912febdb11af111a5d41c1da4bad73d993
Minyoung Lee <minyoung.lee@gmail.com>
2018-10-15 11:58:24 -0700

    Update align_stat.sh

--- align_stat.sh
+++ align_stat.sh
@@ -2,14 +2,23 @@
 
 # Align to Reference genome
 mkdir result_bwa_RST2
-~/bwa/bwa mem -x ont2d fasta_BB_RST2/BbuRST2.180215.fasta  ./fastq/U2T_RST2.fastq > ./result_bwa_RST2/U2T_RST2.sam
+~/bwa/bwa mem -x ont2d fasta_BB_RST2/BbuRST2.180215.fasta  ./fastq/U2T_RST2.fastq > ./result_bwa_RST2/RST2.sam
 
 
 # Alignment stats
 cd result_bwa_RST2
-samtools view -Sb U2T_RST2.sam > tmp.bam
-samtools sort tmp.bam > U2T_RST2.bam
+samtools view -Sb RST2.sam > tmp.bam
+samtools sort tmp.bam > RST2.bam
 rm tmp.bam
-samtools flagstat U2T_RST2.bam > flagstat_RST2.txt
-samtools index U2T_RST2.bam
-samtools idxstats U2T_RST2.bam > idxstats_RST2.txt
+samtools flagstat RST2.bam > flagstat_RST2.txt
+samtools index RST2.bam
+samtools idxstats RST2.bam > idxstats_RST2.txt
+
+
+# reads mapped to RST2 in a bam file
+grep -P "chr|lp|cp" <(samtools view RST2.bam) > readsMapped2RST2.txt
+
+cut -f3 readsMapped2RST2.txt > RNAME.txt
+cut -f4 readsMapped2RST2.txt > POS.txt
+cut -f5 readsMapped2RST2.txt > MAPQ.txt
+cut -f6 readsMapped2RST2.txt > CIGAR.txt
